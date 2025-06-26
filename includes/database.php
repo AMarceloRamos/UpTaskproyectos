67973fac8748f82364a6fc4f06dce4415940cbcf
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-06-25 20:30:16 -0400

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,24 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', 'admin1234', 'upTask');
+function conectarDB(): PgSql\Connection
+{
+    // Cargando las credenciales desde las variables de entorno
+    $host     = getenv('DB_HOST')     ?: 'dpg-d1e8tkili9vc739q0d0g-a';
+    $port     = getenv('DB_PORT')     ?: '5432'; // Puerto por defecto para PostgreSQL
+    $user     = getenv('DB_USER')     ?: 'databaseuptask_user';
+    $password = getenv('DB_PASSWORD') ?: 'AAcMRuVKrgLfiJXRuSGzWLU4qpG7773G';
+    $database = getenv('DB_NAME')     ?: 'databaseuptask';
 
-if (!$db) {
-    echo "Error: No se pudo conectar a MySQL.";
-    echo "errno de depuración: " . mysqli_connect_errno();
-    echo "error de depuración: " . mysqli_connect_error();
-    exit;
+    // Cadena de conexión
+    $connectionString = "host=$host port=$port dbname=$database user=$user password=$password";
+
+    // Conectando a la base de datos
+    $db = pg_connect($connectionString);
+
+    // Verificar si hubo un error de conexión
+    if (!$db) {
+        die("Error: No se puede conectar a la base de datos PostgreSQL.");
+    }
+
+    return $db;
 }
